8c4a62f8f005e7781524cd80334bcf6b8311b779
Banjirsultana90 <banjirsultana15@gmail.com>
2023-11-06 17:40:32 +0600

    create database collection and get data fro database

--- index.js
+++ index.js
@@ -23,14 +23,19 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const alljobs=client.db('jobDB').collection('categories')
+    app.get('/categories',async(req,res)=>{
+        const result=await alljobs.find().toArray()
+        res.send(result)
+    })
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
